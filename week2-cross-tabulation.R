14820be3e71076a906a1ef26fa6d3d256a0827ee
Samah Abdelaal <samah011301@medicine.zu.edu.eg>
2020-05-05 22:14:56 +0200

    finished cross tabulation quiz

--- week2-cross-tabulation.R
+++ week2-cross-tabulation.R
@@ -7,6 +7,7 @@ g <- read.csv(file = "Data/final-diabetes-data-for-R-_csv_-_2_.csv",
 colnames(g)
 
 age <- g$age
+gender <- g$gender
 
 library(tidyverse)
 age_categorized <- case_when(age < 45 ~ "under 45",
@@ -14,9 +15,18 @@ age_categorized <- case_when(age < 45 ~ "under 45",
                              age >= 65 & age < 75 ~ "65-74",
                              age >= 75 ~ "75 or over",
                              TRUE ~ NA)
-<<<<<<< HEAD
 
 # Error: must be a character vector, not a logical vector !!!
 
-=======
->>>>>>> d3e15191481b124e5f9544d014ed7c5ee0686e28
+age_categorized <- ifelse(age < 45, "< 45",
+                          ifelse(age >= 45 & age < 65, "45-64",
+                                 ifelse(age >= 65 & age < 75, "65-74",
+                                        ifelse(age >= 75, "75 or over", NA))))
+table(age_categorized, exclude = NULL)
+# no. of females aged under 45 is 126
+# no missing values
+
+age_by_gender <- table(age_categorized, gender, exclude = NULL)
+
+round(100*prop.table(age_by_gender, margin = 2))
+# pct of ALL PEOPLE who are male and aged 65-75 ia 5%
